f27071c483c9d75e5cd479eb2d6122a17a1f373e
Filipe Yamamoto <fhtyamamoto@gmail.com>
2020-12-02 18:25:45 -0300

    Change footer

--- component/footer.js
+++ component/footer.js
@@ -5,7 +5,7 @@ export default (props) => (
     <li>Compartilhe:</li>
     <li>
       <a
-        href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fshouldideploy.today%2F&t=Should%20I%20Deploy%20Today%3F"
+        href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fvaidarpraia.vercel.app%2F%26t%3DVai%20dar%20praia%3F"
         target="_blank"
         rel="noopener noreferrer"
         title="Share on Facebook"
@@ -15,7 +15,7 @@ export default (props) => (
     </li>
     <li>
       <a
-        href="https://twitter.com/intent/tweet?source=http%3A%2F%2Fshouldideploy.today%2F&text=Should%20I%20Deploy%20Today%3F:%20http%3A%2F%2Fshouldideploy.today"
+        href="https://twitter.com/intent/tweet?source=https%3A%2F%2Fvaidarpraia.vercel.app%2F%26text%3DVai%20dar%20praia%3F%3A%20https%3A%2F%2Fvaidarpraia.vercel.app%2F"
         target="_blank"
         rel="noopener noreferrer"
         title="Tweet"
